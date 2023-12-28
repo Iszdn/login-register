972a25e0326cb5830a464f075bc708d9a1bcd3db
Iszdn <tu7k21xbe@code.edu.az>
2023-12-29 01:02:21 +0400

    delete user by admin

--- client/src/Pages/AdminPage/index.jsx
+++ client/src/Pages/AdminPage/index.jsx
@@ -1,32 +1,39 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const AdminPage = () => {
-
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   async function getUsers() {
-    const res=await axios("http://localhost:5000/")
-    const data=res.data
-    setUsers(data)
+    const res = await axios.get("http://localhost:5000/");
+    const data = res.data;
+    setUsers(data);
   }
+
+  async function deleteUser(id) {
+    try {
+      await axios.delete(`http://localhost:5000/${id}`);
+      getUsers(); 
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  }
+
   useEffect(() => {
-    getUsers()
-  }, [])
-  
+    getUsers();
+  }, []);
 
   return (
     <>
-{
-  users && users.map((x)=>(
-    <div className='users'>
-    <h3>{x.username}</h3> <button >delete user</button>
-    </div>
-    
-  ))
-}
+      {users &&
+        users.map((user) => (
+          <div className='users' key={user._id}>
+            <h3>{user.username}</h3>
+            <button onClick={() => deleteUser(user._id)}>Delete User</button>
+          </div>
+        ))}
     </>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -1,14 +1,14 @@
-import { Router } from 'express'
-import { deleteUser, getUser, userLogin, userRegister } from '../controllers/users.js'
-import { adminAuth } from '../middleware/auth.js'
-const router = Router()
+    import { Router } from 'express'
+    import { deleteUser, getUser, userLogin, userRegister } from '../controllers/users.js'
+    import { adminAuth } from '../middleware/auth.js'
+    const router = Router()
 
 
-router.post("/register",userRegister)
-router.post("/login",userLogin)
-router.delete("/:id",adminAuth,deleteUser)
-router.get("/",getUser)
+    router.post("/register",userRegister)
+    router.post("/login",userLogin)
+    router.delete("/:id",adminAuth,deleteUser)
+    router.get("/",getUser)
 
 
 
-export default router
+    export default router
